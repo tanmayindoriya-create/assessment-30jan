60b30998676aa376bcb223c6f41b96d5aec806bf
TanmayIndoriya <tanmayindoriya@gmail.com>
2026-01-30 15:41:05 +0530

    Complete Assessment solutions - Solved 5 SQL challenges and 1 python challenge

--- SQL-Challenge/Problem5/solution5.sql
+++ SQL-Challenge/Problem5/solution5.sql
@@ -1,13 +1,12 @@
-with cte as (
-	select
+select
+    row_id,
+    max(job_role) over(partition by grp) as job_role,
+    skills
+from (
+    select
         row_id,
         job_role,
         skills,
         sum(case when job_role is not null then 1 else 0 end) over (order by row_id) as grp
     from job_skills
-)
-select
-    row_id,
-    max(job_role) over(partition by grp) as job_role,
-    skills
-from cte
+) t
